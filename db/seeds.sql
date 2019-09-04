eb95a8dff7d16d222504a20ba193091a622d7156
Nary Hang <naryhang@gmail.com>
2019-09-03 21:26:34 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+--Seeds file to populate database with some starter data--
+
+INSERT INTO burgers(burger_name, devoured)
+	values('Black Bean Burger', false),
+    ('Chicken Burger', false),
+    ('Beef Burger', false);
+    
+    select * from burgers;
