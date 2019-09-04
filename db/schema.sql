218b054e4caa486e57f329738943fdadc0d66784
Nary Hang <naryhang@gmail.com>
2019-09-04 16:54:08 -0700

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,10 @@
---Create database named burger_db and use it--
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
---Create table named burgers with columns form id, burger_name, and devoured.--
---Make the id column the primary key--
-
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-    devoured boolean NOT NULL,
-	date TIMESTAMP not null DEFAULT current_timestamp,
+CREATE TABLE burgers (
+	id INTEGER(20) AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(100),
+	devoured BOOLEAN DEFAULT FALSE,
+	date TIMESTAMP,
 	PRIMARY KEY (id)
 );
