d2f343f6089c9a71000ca73db69988edf3a3b713
Nary Hang <naryhang@gmail.com>
2019-09-04 16:56:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-//Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-//Define port the server will be listening on.
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-//Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Parse application/json
-app.use(bodyParser.json());
-
-//Set Handlebars.
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
-//App is listening...
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
-});
+app.listen(PORT);
