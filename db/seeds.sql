8ad3ed9e03dab18a7de4d7c91fb70e3a68e60017
Nary Hang <naryhang@gmail.com>
2019-09-04 16:54:28 -0700

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,4 @@
---Seeds file to populate database with some starter data--
-
-INSERT INTO burgers(burger_name, devoured)
-	values('Black Bean Burger', false),
-    ('Chicken Burger', false),
-    ('Beef Burger', false);
-    
-    select * from burgers;
+INSERT INTO burgers (burger_name) VALUES ("classic burger");
+INSERT INTO burgers (burger_name) VALUES ("avocado bacon burger");
+INSERT INTO burgers (burger_name) VALUES ("mushroom onion burger");
+INSERT INTO burgers (burger_name) VALUES ("veggie burger");
